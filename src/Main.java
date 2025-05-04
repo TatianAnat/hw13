17513a1e79c0ce4d454812f05be9faabddb7c37a
TatianAnat <TatianAnat@yandex.ru>
2025-05-04 13:39:07 +0400

    homework 13 task 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1");
         DeterminesLeapYear(2021);
-                }
-//task 1
-    public static void DeterminesLeapYear(int year){
+
+        System.out.println("Task 2");
+        phoneAppVersion(0,2015);
+    }
+
+    //task 1
+    public static void DeterminesLeapYear(int year) {
 
         if (year > 1584) {
             if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-                System.out.println(year+" год - високосный год");
+                System.out.println(year + " год - високосный год");
             } else {
                 System.out.println(year + " год - невисокосный год");
             }
@@ -17,4 +23,25 @@ public class Main {
 
     }
 
+    //task2
+    public static void phoneAppVersion(int clienOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (clienOS == 1) {
+            if (clientDeviceYear >= currentYear) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+
+            }
+        }
+        if (clienOS == 0) {
+            if (clientDeviceYear >= currentYear) {
+                System.out.println("Установите версию приложения для iOS  по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+
+    }
 }
