f046ccddf304c93b3a685ae6610d38664effaf79
TatianAnat <TatianAnat@yandex.ru>
2025-05-04 12:45:55 +0400

    homework 13 task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Task 1");
+        DeterminesLeapYear(2021);
+                }
+//task 1
+    public static void DeterminesLeapYear(int year){
+
+        if (year > 1584) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year+" год - високосный год");
+            } else {
+                System.out.println(year + " год - невисокосный год");
+            }
+        }
+
+
     }
+
 }
